7ccb20d27ff651e224d6f1b3064f7377aa1b107c
Brandon DuBois <skrilled@gmail.com>
2025-05-13 23:49:23 -0400

    plugin comment updates

--- wp-location-redirect.php
+++ wp-location-redirect.php
@@ -1,9 +1,17 @@
 <?php
 /*
 Plugin Name: WP Location Redirect
-Description: Redirect users based on their location, such as country, state, city by using the GeoLite2-City database.
+Plugin URI: https://yourwebsite.com/wp-location-redirect
+Description: Redirect users based on their location, such as country, state, and city, using the GeoLite2-City database.
 Version: 1.0.0
+Requires at least: 5.6
+Requires PHP: 7.0
 Author: Brandon DuBois
+Author URI: https://www.nerdysouthinc.com
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wp-location-redirect
+Tags: location, redirect, geolocation, GeoLite2, MaxMind
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
